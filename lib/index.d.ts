7a729795565bf94a148ced861b32645a72adf444
Maksim Skavysh <maksim.skavysh@gmail.com>
2019-11-21 19:56:36 +0300

    Add reducer with path

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,8 @@
 declare type reducerType = (state: any, action: any) => any;
+export declare const EMPTY_OBJECT: Readonly<{}>;
+export declare const EMPTY_ARRAY: readonly never[];
 export declare const composeReducers: (...reducers: reducerType[]) => (state: any, action: any) => any;
+export declare const reducerWithPath: (reducer: reducerType, getPath: (arg: any) => (string | number)[]) => (state: any, action: any) => any;
 export declare const commonActionCreator: (module: string) => (name: string, payloadValidator?: ((data: any) => void) | undefined, metaValidator?: ((data: any) => void) | undefined) => {
     (payload: any, meta: void | object): {
         type: string;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-
+import * as T from 'runtypes'
+import * as R from 'ramda'
 type reducerType = (state: any, action: any) => any
 type validatorType = (data: any) => void
 
+export const EMPTY_OBJECT = Object.freeze({})
+export const EMPTY_ARRAY = Object.freeze([])
+
 export const composeReducers = (...reducers: reducerType[]) => (state: any, action: any) =>
     reducers.reduceRight((curState, reducer) => reducer(curState, action), state)
+
+
+const idType = T.String.withConstraint(s => s.length > 0 || 'Empty id').Or(T.Number)
+const pathType = T.Array(idType).withConstraint(a => a.length > 0 || 'path with length 0 is not allowed')
+const valueType = T.Unknown
+const idValueType = T.Record({ id: idType, value: valueType })
+const pathValueType = T.Record({ path: pathType, value: valueType })
+
+export const reducerWithPath = (reducer: reducerType, getPath: (arg: any) => (string | number)[]) =>
+    (state: any, action: any) => {
+        let path = getPath(action)
+        if (path) {
+            pathType.check(path)
+            const inner = R.path(path, state)
+            return R.assocPath(path, reducer(inner, action), state)
+        }
+        return state || EMPTY_OBJECT
+    }
 
 export const commonActionCreator = (module: string) =>
     (name: string, payloadValidator?: (data: any) => void, metaValidator?: (data: any) => void) => {
